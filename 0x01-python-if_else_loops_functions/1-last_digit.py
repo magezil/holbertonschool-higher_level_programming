5ebe3642e8d0644e5e580db955487dcf450b3c6d
magezil <256@holbertonschool.com>
2017-12-05 18:42:02 +0000

    print combination, style updates

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,9 +2,9 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-	sign = -1
+    sign = -1
 else:
-	sign = 1
+    sign = 1
 digit = sign * ((sign * number) % 10)
 print("Last digit of {:d} is {:d} and is".format(number, digit), end=" ")
 if digit > 5:
