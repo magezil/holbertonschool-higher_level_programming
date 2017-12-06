30166dfea0e566b334e0e7b6bd29022ae05c3936
magezil <256@holbertonschool.com>
2017-12-06 04:56:59 +0000

    re-add old neg number logic

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-digit = number % 10
+if number < 0:
+    sign = -1
+else:
+    sign = 1
+digit = sign * (sign * number % 10)
 print("Last digit of {:d} is {:d} and is".format(number, digit), end=" ")
 if digit > 5:
     print("greater than 5")
