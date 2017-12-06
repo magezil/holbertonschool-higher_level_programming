826b15f0c7e87cf601d77fdee81a6e835d6b0e41
magezil <256@holbertonschool.com>
2017-12-06 05:24:53 +0000

    take advantage of format

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 for i in range(99):
-    print("{:d} = 0x".format(i), end="")
-    if i // 16 >= 1:
-        print(i // 16, end="")
-        i %= 16
-    if i >= 10 and i <= 15:
-        print("abcdef"[i % 10])
-    else:
-        print(i % 16)
+    print("{:d} = 0x{:x}".format(i, i))
