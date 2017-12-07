edd4f30fe3a45808ec4b246a4e35629019b30510
magezil <256@holbertonschool.com>
2017-12-07 22:12:28 +0000

    make not execute on import

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-from hidden_4 import *
-for d in dir():
-    if d[0] != "_":
-        print("{}".format(d))
+if __name__ == "__main__":
+    from hidden_4 import *
+    for d in dir():
+        if d[0] != "_":
+            print("{}".format(d))
