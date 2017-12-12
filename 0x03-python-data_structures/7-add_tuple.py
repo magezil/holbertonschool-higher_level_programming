a4941391b7c6c89e27c2f5cf32a909ae4c0d72b1
magezil <256@holbertonschool.com>
2017-12-12 20:39:23 +0000

    correct tuple_b=(x,) logic

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -9,7 +9,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_b) == 0:
         c, d = 0, 0
     elif len(tuple_b) == 1:
-        c, d = tuple_a[0], 0
+        c, d = tuple_b[0], 0
     else:
         c, d = tuple_b
     return (a + c, b + d)
