648e91964ad8ca8f2d28b46624893b138b2ed800
magezil <256@holbertonschool.com>
2017-12-13 00:13:57 +0000

    change algo

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,6 +1,52 @@
 #include "lists.h"
 
 /**
+ * reverse_list - reverses a singly linked list
+ * @head: head of listint_t linked list
+ *
+ * Return: head of reversed listint_t linked list
+ */
+listint_t *reverse_list(listint_t *head)
+{
+	listint_t *new;
+
+	new = NULL;
+	if (head == NULL)
+		return (NULL);
+	while (head != NULL)
+	{
+		new = add_nodeint(&new, head->n);
+		head = head->next;
+	}
+	return (new);
+}
+
+/**
+ * add_nodeint - function adds a new node to the beginning of a listint_t list
+ * @head: pointer to pointer to list
+ * @n: n for new node
+ *
+ * Return: address to new element or NULL if failed
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	if (*head == NULL)
+		new->next = NULL;
+	else
+		new->next = *head;
+	*head = new;
+	return (new);
+}
+
+/**
  * is_palindrome - checks if a singly linked list is a palindrome
  * @head: head of listint_t linked list
  *
@@ -8,32 +54,20 @@
  */
 int is_palindrome(listint_t **head)
 {
+	listint_t *rev;
 	listint_t *temp;
-	listint_t *end;
-	int flag = 1;
 
-	/* invalid or empty list */
 	if (head == NULL || *head == NULL)
 		return (1);
-	end = (*head)->next;
-	/* check 1 node */
-	if (end == NULL)
-		return (1);
 	temp = *head;
-	/* check 2 nodes */
-	if (end->next == NULL && temp->n == end->n)
-		return (1);
-	while (end->next != NULL)
+	rev = reverse_list(*head);
+	while (temp != NULL && rev != NULL)
 	{
-		temp = end;
-		end = end->next;
+		if (temp->n != rev->n)
+			return (0);
+		temp = temp->next;
+		rev = rev->next;
 	}
-	if ((*head)->n == end->n)
-	{
-		temp->next = NULL;
-		flag = flag && is_palindrome(&(*head)->next);
-		temp->next = end;
-		return (flag);
-	}
-	return (0);
+
+	return (1);
 }
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,7 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -22,5 +22,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
+listint_t *reverse_list(listint_t *head);
+listint_t *add_nodeint(listint_t **head, const int n);
 
 #endif /* LISTS_H */
