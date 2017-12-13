bd1cf01fc9e2133297bc568ad70b077125bc6001
magezil <256@holbertonschool.com>
2017-12-13 05:38:54 +0000

    clean up code

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -9,39 +9,6 @@
  */
 int is_palindrome(listint_t **head)
 {
-/*
- *	listint_t *temp;
- *	listint_t *end;
- *	int flag;
-*/
-	/* invalid or empty list */
-/*
- *	if (head == NULL || *head == NULL)
- *		return (1);
-*/	/* check 1 node */
-/*
- *	if ((*head)->next == NULL)
- *		return (1);
- *	end = (*head)->next;
- *	temp = *head;
-*/	/* check 2 nodes */
-/*
- *	if (end->next == NULL && temp->n == end->n)
- *		return (1);
- *	while (end->next != NULL)
- *	{
- *		temp = end;
- *		end = end->next;
- *	}
- *	if ((*head)->n == end->n)
- *	{
- *		temp->next = NULL;
- *		flag = 1 && is_palindrome(&(*head)->next);
- *		temp->next = end;
- *		return (flag);
- *	}
- *	return (0);
- */
 	if (head == NULL || *head == NULL || (*head)->next == NULL)
 		return (1);
 	return (is_pal_helper(head, *head));
@@ -56,12 +23,9 @@ int is_palindrome(listint_t **head)
  */
 int is_pal_helper(listint_t **head, listint_t *end)
 {
-	int flag;
-
-	if (head == NULL || *head == NULL || (*head)->next == NULL || end == NULL)
+	if (end == NULL)
 		return (1);
-	flag = is_pal_helper(head, end->next);
-	if (flag && (*head)->n == end->n)
+	if (is_pal_helper(head, end->next) && (*head)->n == end->n)
 	{
 		*head = (*head)->next;
 		return (1);
