11801d2b695294962cbdcf5294fa36ff2a68b1b0
magezil <256@holbertonschool.com>
2017-12-14 18:59:10 +0000

    sum unique without import

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     if my_list:
-        from functools import reduce
-        return reduce(lambda x, y: x + y, set(my_list))
+        return sum(set(my_list))
