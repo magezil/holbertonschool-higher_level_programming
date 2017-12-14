60a7cbdc9357e71a8d2b2dc4805c610b515f05aa
magezil <256@holbertonschool.com>
2017-12-14 20:41:54 +0000

    remove if valid checks

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list and search:
-        return [replace if x == search else x for x in my_list]
+    return [replace if x == search else x for x in my_list]
