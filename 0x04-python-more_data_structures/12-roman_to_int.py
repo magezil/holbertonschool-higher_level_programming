cc084eb682706cc7d3ce6ad3676dbaa24f89d0e3
magezil <256@holbertonschool.com>
2017-12-14 22:46:22 +0000

    fix check valid

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string or type(roman_string) is str:
+    if roman_string and type(roman_string) is str:
         roman_dict = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
         value = 0
         for c, n in zip(roman_string, roman_string[1:]):
