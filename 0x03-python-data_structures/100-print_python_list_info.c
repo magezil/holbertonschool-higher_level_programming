070de8d32a8a27b6875c256832cd50424f855705
magezil <256@holbertonschool.com>
2017-12-14 00:31:50 +0000

    C Python, fix typos, check valid

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -9,8 +9,10 @@ void print_python_list_info(PyObject *p)
 	PyListObject *listobj;
 	int i;
 
+	if (!p)
+		return;
 	listobj = (PyListObject *) p;
-	printf("[*] Size of Python List = %d\n", (int)PyList_Size(p));
+	printf("[*] Size of the Python List = %d\n", (int)PyList_Size(p));
 	printf("[*] Allocated = %d\n", (int)listobj->allocated);
 	for (i = 0; i < Py_SIZE(p); i++)
 		printf("Element %d: %s\n", i, listobj->ob_item[i]->ob_type->tp_name);
