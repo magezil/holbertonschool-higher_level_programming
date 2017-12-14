95ab5149298e3fdfa18748d3332daa9c95f09d39
magezil <256@holbertonschool.com>
2017-12-14 18:32:36 +0000

    using list comprehension

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     if matrix:
-        new_matrix = []
-        for row in matrix:
-            new_matrix.append(list(map(lambda x: x**2, row)))
-        return new_matrix
+        return [list(map(lambda x: x**2, row)) for row in matrix]
