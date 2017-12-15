190cba499a5f7e23c5f89ef47a2cb71c9fb2e96d
magezil <256@holbertonschool.com>
2017-12-15 19:17:25 +0000

    initial version

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,51 @@
+#include "Python.h"
+#include "bytesobject.h"
+#include <string.h>
+
+/**
+ * print_python_list - prints basic info about Python lists
+ * @p: Python object (list)
+ */
+void print_python_list(PyObject *p)
+{
+	PyListObject *listobj;
+	ssize_t i;
+
+	if (!p)
+		return;
+	listobj = (PyListObject *) p;
+	printf("[*] Size of the Python List = %d\n", (int)PyList_Size(p));
+	printf("[*] Allocated = %d\n", (int)listobj->allocated);
+	for (i = 0; i < PyList_Size(p); i++)
+		printf("Element %zd: %s\n", i, listobj->ob_item[i]->ob_type->tp_name);
+}
+
+/**
+ * print_python_bytes - prints basic info about Python bytes
+ * @p: Python object (bytes)
+ */
+void print_python_bytes(PyObject *p)
+{
+/*	PyBytesObject *bytesobj;
+*/	ssize_t i;
+	ssize_t size;
+	char *string;
+
+	printf("[.] bytes object info\n");
+	if (!p)
+	{
+		printf("  [ERROR] Invalid Bytes Object");
+		return;
+	}
+/*	bytesobj = (PyBytesObject *) p;
+*/	size = (ssize_t)PyBytes_Size(p);
+	string = (char *)PyBytes_AsString(p);
+	printf("size: %zd\n", size);
+	printf("trying string: %s\n", string);
+	if (size > 10)
+		size = 10;
+	printf("first %zd bytes: ", size);
+	for (i = 0; i < size; i++)
+		printf("%0hhx ", string[i]);
+	printf("\n");
+}
