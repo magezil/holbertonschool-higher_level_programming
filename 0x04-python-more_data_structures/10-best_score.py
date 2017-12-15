9fc98a6e0cb02ce1689f12a3ccdc8cd2f4189b6f
magezil <256@holbertonschool.com>
2017-12-15 19:13:40 +0000

    one line after PLD

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def best_score(my_dict):
-    highest = None
-    scorer = None
-    if my_dict:
-        for k, v in my_dict.items():
-            if highest is None or v > highest:
-                highest = v
-                scorer = k
-    return scorer
+    return max(my_dict) if my_dict else None
