d2d8b8ee02e48a560d546186976634b2ae744259
magezil <256@holbertonschool.com>
2018-01-02 21:27:48 +0000

    account for negative x

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if not my_list:
-        return 0
     try:
-        for i in range(x):
+        i = 0
+        while i < x:
             print(my_list[i], end="")
-        i += 1
+            i += 1
+    except IndexError:
+        pass
+    finally:
         if i > 0:
             print()
-    except IndexError:
-        print()
-    return i
+        return i
