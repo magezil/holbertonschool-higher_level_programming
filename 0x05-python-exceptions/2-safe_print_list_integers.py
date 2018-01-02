1d499acc87b64ab7c6d60dd2095ce0035c11c45e
magezil <256@holbertonschool.com>
2018-01-02 23:32:55 +0000

    add two exceptions doing same thing to same line

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,9 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             ints += 1
-        except ValueError:
-            pass
-        except TypeError:
+        except (ValueError, TypeError):
             pass
         i += 1
     print("")
