703755e0914460d99e7cb98970eb62cfdce25083
magezil <256@holbertonschool.com>
2018-01-02 21:58:36 +0000

    allow IndexError

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,8 +6,6 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             ints += 1
-        except IndexError:
-            pass
         except ValueError:
             pass
         except TypeError:
