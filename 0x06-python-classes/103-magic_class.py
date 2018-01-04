4e401ad70f6b1a0ad4bb8ee871f7afcc362741d8
magezil <256@holbertonschool.com>
2018-01-04 05:25:26 +0000

    remove property

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -3,23 +3,15 @@
 
 class MagicClass:
     """Magic class that does the same as given bytecode (Circle)"""
-    def __init__(self, radius=None):
-        """Initialize radius"""
-        self.radius = radius
+    def __init__(self, radius=0):
+        """Initialize radius
 
-    @property
-    def radius(self):
-        """radius: radius of circle
-
-        setter validates radius is a number
+        Args:
+            radius: radius of circle
 
         Raises:
             TypeError: If radius is not an int nor a float
         """
-        return self.__radius
-
-    @radius.setter
-    def radius(self, radius):
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         else:
@@ -27,8 +19,8 @@ class MagicClass:
 
     def area(self):
         """Returns the calculated area of circle"""
-        return self.radius ** 2 * math.pi
+        return self.__radius ** 2 * math.pi
 
     def circumference(self):
         """Returns the calculated circumference of circle"""
-        return 2 * math.pi * self.radius
+        return 2 * math.pi * self.__radius
