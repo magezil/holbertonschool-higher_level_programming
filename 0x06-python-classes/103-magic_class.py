abb57a03178decdd7643bd4083a8d33393a828c8
magezil <256@holbertonschool.com>
2018-01-04 05:17:33 +0000

    add docstrings and pep8 compliant

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 
+
 class MagicClass:
+    """Magic class that does the same as given bytecode (Circle)"""
     def __init__(self, radius=None):
+        """Initialize radius"""
         self.radius = radius
 
     @property
     def radius(self):
+        """radius: radius of circle
+
+        setter validates radius is a number
+
+        Raises:
+            TypeError: If radius is not an int nor a float
+        """
         return self.__radius
 
     @radius.setter
@@ -16,7 +26,9 @@ class MagicClass:
             self.__radius = radius
 
     def area(self):
+        """Returns the calculated area of circle"""
         return self.radius ** 2 * math.pi
 
     def circumference(self):
+        """Returns the calculated circumference of circle"""
         return 2 * math.pi * self.radius
