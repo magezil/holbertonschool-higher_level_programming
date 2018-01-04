45bea5e76ba370e2a48d0650ad90ce009fe10c10
magezil <256@holbertonschool.com>
2018-01-04 01:47:45 +0000

    edit to take advantage of property

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -8,12 +8,7 @@ class Square:
         """Args:
                size: size of square
         """
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.size = size
 
     @property
     def size(self):
