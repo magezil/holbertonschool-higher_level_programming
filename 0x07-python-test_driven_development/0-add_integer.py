8e44f78da79f1e00075633f9e4bfd2c31351ce66
magezil <256@holbertonschool.com>
2018-01-06 02:42:30 +0000

    add_integer tests, function, README

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module defines the function add_integer(). For example,
+Raises:
+    TypeError: if a or b are not a number (integer or float)
+"""
+def add_integer(a, b):
+    """
+    Returns the result of a + b
+    """
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
