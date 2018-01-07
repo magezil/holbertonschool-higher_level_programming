536dd3c292554b9500ccd36b97b5c1181f10dbb2
magezil <256@holbertonschool.com>
2018-01-07 01:10:12 +0000

    account for ending with whitespace

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -13,9 +13,16 @@ def text_indentation(text):
     if type(text) is not str:
         raise TypeError("text must be a string")
     start = True
+    space_count = 0
     for c in text:
         if c == " " and start:
             continue
+        if c == " ":
+            space_count += 1
+            continue
+        else:
+            print("{}".format(" " * space_count), end="")
+            space_count = 0
         if c == "." or c == "?" or c == ":":
             print("{}\n".format(c))
             start = True
