a55603f2c0e7afa08a456e283d293bbefe267473
magezil <256@holbertonschool.com>
2018-01-17 03:35:16 +0000

    add Rectangle init

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""This module creates class Rectangle which inherits from BaseGeometry"""
+
+
+class BaseGeometry:
+    """Class BaseGeometry"""
+    def area(self):
+        """Raises an Exception - method not implemented"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates a given value is a positive int
+
+        Param:
+            name: name of variable to validate
+            value: value to validate
+
+        Raises:
+            TypeError: if value is not an int
+            ValueError: if value is negative
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value < 0:
+            raise ValueError("{} must be greater than 0". format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Class Rectangle"""
+    def __init__(self, width, height):
+        """Instantiate private instance fields width and height"""
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
