3130dd5d0601b2b21c5831a33239f1fe587ae518
magezil <256@holbertonschool.com>
2018-01-17 22:35:11 +0000

    pep8 style compliant

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,7 +3,11 @@
 
 
 def read_file(filename=""):
-    """This function reads a text file in UTF8 encoding and prints it"""
+    """This function reads a text file in UTF8 encoding and prints it
+
+    Param:
+        filename: name of file to read from
+    """
     with open(filename, encoding="UTF8") as f:
         for line in f:
             print(line, end="")
--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -3,7 +3,11 @@
 
 
 def number_of_lines(filename=""):
-    """Returns number of lines in given text file"""
+    """Returns number of lines in given text file
+
+    Param:
+        filename: name of file to read from
+    """
     count = 0
     with open(filename, encoding="UTF8") as f:
         for line in f:
