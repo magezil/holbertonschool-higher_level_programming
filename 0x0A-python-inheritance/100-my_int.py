9a1efc78ba79ab3582b801b975c2025ff9d5be92
magezil <256@holbertonschool.com>
2018-01-17 05:32:22 +0000

    renegade MyInt class

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+class MyInt(int):
+    """Class MyInt is a rebel and inverts == and !="""
+    def __eq__(self, other):
+        """Returns opposite of what int eq() would return"""
+        return not super().__eq__(other)
+
+
+    def __ne__(self, other):
+        """Returns opposite of what int ne() would return"""
+        return not super().__ne__(other)
