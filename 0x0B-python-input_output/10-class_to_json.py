b22af83444f4bc212ba7a8da4b8e97d1baf9ea85
magezil <256@holbertonschool.com>
2018-01-18 02:03:04 +0000

    return class dictionary

---
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module defines function class_to_json"""
+import json
+
+
+def class_to_json(obj):
+    """Returns the dictionary description of simple data structure
+    (list, dictionary, string, integer, boolean)
+
+    Param:
+        obj: instance of a class
+    """
+    return obj.__dict__
