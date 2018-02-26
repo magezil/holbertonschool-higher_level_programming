5a6f91331bce3a6334631a07fb9f58a3894c99cb
magezil <256@holbertonschool.com>
2018-02-26 22:05:01 +0000

    Simplify query

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- Create new table 'first_table' in current database
-CREATE TABLE IF NOT EXISTS first_table (id INT(11) DEFAULT NULL, name VARCHAR(256) DEFAULT NULL);
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
