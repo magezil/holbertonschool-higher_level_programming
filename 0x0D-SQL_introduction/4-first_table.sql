616441c67000c81deeab8edf73d72feea98055cc
magezil <256@holbertonschool.com>
2018-02-26 21:21:44 +0000

    Add check for table exists

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- Create new table 'first_table' in current database
-CREATE TABLE first_table (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256));
