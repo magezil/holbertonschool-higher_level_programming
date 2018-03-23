4e2b727eae73b7f1fccf4a8d1bbdc12f235ed02f
magezil <256@holbertonschool.com>
2018-03-23 01:22:58 +0000

    Add blank lines between code and import

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,6 +10,8 @@ from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from sys import argv
+
+
 Session = sessionmaker()
 engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
     argv[1], argv[2], argv[3]), pool_pre_ping=True)
