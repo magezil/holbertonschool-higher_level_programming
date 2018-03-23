89313e96e5a962e48291bb3ac3fd385dfd1ad603
magezil <256@holbertonschool.com>
2018-03-23 01:24:50 +0000

    Remove blank lines, add check for import

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -6,16 +6,15 @@ Takes three arguments:
     database name
 Connects to default host (localhost) and port (3306)
 """
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-from sys import argv
-
-
-Session = sessionmaker()
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-    argv[1], argv[2], argv[3]), pool_pre_ping=True)
-session = Session(bind=engine)
-Base.metadata.create_all(engine)
-for instance in session.query(State).order_by(State.id):
-    print("{}: {}".format(instance.id, instance.name))
+if __name__ == "__main__":
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+    from sys import argv
+    Session = sessionmaker()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    session = Session(bind=engine)
+    Base.metadata.create_all(engine)
+    for instance in session.query(State).order_by(State.id):
+        print("{}: {}".format(instance.id, instance.name))
