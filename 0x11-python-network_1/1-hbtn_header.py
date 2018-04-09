1d60437593e98ae4e5fa17694d69a5af79159c6c
magezil <256@holbertonschool.com>
2018-04-09 20:25:54 +0000

    Print value of header variable

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    Script sends a request to given URL
+    Displays value of X-Request-Id variable from header of response
+"""
+import urllib.request
+from sys import argv
+url = argv[1]
+local_file, headers = urllib.request.urlretrieve(url)
+#print(headers.getheader('X-Request-Id'))
+for h in headers._headers:
+    if h[0] == 'X-Request-Id':
+        print(h[1])
+#print(headers)
--- 0x11-python-network_1/README.md
+++ 0x11-python-network_1/README.md
@@ -7,3 +7,6 @@
 ### 0-hbtn_status.py
 * Script fetches https://intranet.hbtn.io/status
 
+### 1-hbtn_header.py
+* Script takes URL, sends a request to URL, and display value of X-Request-Id variable from header of response
+
