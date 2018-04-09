1e06ce9beda9ee8c33f94fe733ff20221990d00a
magezil <256@holbertonschool.com>
2018-04-09 23:14:03 +0000

    add data={}

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,12 +7,10 @@ from sys import argv
 if __name__ == "__main__":
     q = argv[1] if len(argv) > 1 else ""
     try:
-        r = requests.post('http://0.0.0.0:5000/search_user', {'q': q}).json()
+        r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q}).json()
+        if 'id' in r and 'name' in r:
+            print("[{}] {}".format(r['id'], r['name']))
+        else:
+            print("No result")
     except ValueError:
-        print("Not a valid JSON")
-    if 'id' in r and 'name' in r:
-        print("[{}] {}".format(r['id'], r['name']))
-    elif len(r) == 0:
-        print("No result")
-    else:
         print("Not a valid JSON")
