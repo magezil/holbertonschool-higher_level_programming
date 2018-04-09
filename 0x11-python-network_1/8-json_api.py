55ed0f8d1b9a8bc45a1a600722ffb158eb01c211
magezil <256@holbertonschool.com>
2018-04-09 23:15:40 +0000

    Fix pep8 errors

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,7 +7,8 @@ from sys import argv
 if __name__ == "__main__":
     q = argv[1] if len(argv) > 1 else ""
     try:
-        r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q}).json()
+        r = requests.post('http://0.0.0.0:5000/search_user',
+                          data={'q': q}).json()
         if 'id' in r and 'name' in r:
             print("[{}] {}".format(r['id'], r['name']))
         else:
