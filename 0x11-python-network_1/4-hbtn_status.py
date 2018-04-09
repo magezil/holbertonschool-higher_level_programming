0a3cdf4a443a1f1509e518736646225b3a3d009d
magezil <256@holbertonschool.com>
2018-04-09 22:04:07 +0000

    Add import protection

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,7 +3,8 @@
     Use package requests to fetch https://intranet.hbtn.io/status
 """
 import requests
-r = requests.get('https://intranet.hbtn.io/status').text
-print("Body response:")
-print("\t- type: {}".format(type(r)))
-print("\t- content: {}".format(r))
+if __name__ == "__main__":
+    r = requests.get('https://intranet.hbtn.io/status').text
+    print("Body response:")
+    print("\t- type: {}".format(type(r)))
+    print("\t- content: {}".format(r))
