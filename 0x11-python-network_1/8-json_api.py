2cb94b47111e5c8294cb4b185a57e61408382eeb
magezil <256@holbertonschool.com>
2018-04-09 23:00:38 +0000

    Try/except for Invalid JSON Exception

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,10 +6,11 @@ import requests
 from sys import argv
 if __name__ == "__main__":
     q = argv[1] if len(argv) > 1 else ""
-    r = requests.post('http://0.0.0.0:5000/search_user', {'q': q}).json()
+    try:
+        r = requests.post('http://0.0.0.0:5000/search_user', {'q': q}).json()
+    except ValueError:
+        print("Not a valid JSON")
     if 'id' in r and 'name' in r:
         print("[{}] {}".format(r['id'], r['name']))
-    elif len(r) == 0:
-        print("No result")
     else:
-        print("Not a valid JSON")
+        print("No result")
