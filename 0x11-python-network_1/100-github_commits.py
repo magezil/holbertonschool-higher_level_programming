277af75b5556f2c93d45af351cdffbc5071e6bf1
magezil <256@holbertonschool.com>
2018-04-10 01:10:09 +0000

    Fix pep8 erros

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,6 +7,7 @@ import requests
 from sys import argv
 repo = argv[1]
 owner = argv[2]
-r = requests.get('https://api.github.com/repos/{}/{}/commits'.format(owner, repo)).json()
+r = requests.get('https://api.github.com/repos/{}/{}/commits'
+                 .format(owner, repo)).json()
 for i in range(10):
     print("{}: {}".format(r[i]["sha"], r[i]["commit"]["author"]["name"]))
