56c90958fe972aa541dc6c49713b57b5a30173fb
Liz M <256@holbertonschool.com>
2018-05-07 21:39:32 +0000

    Inherit from Square not from Rectangle

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const Square0 = require('./5-square');
 
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+class Square extends Square0 {
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
