3ec978657e2faf235d73f9134cfa5528e0c33d51
Liz M <256@holbertonschool.com>
2018-05-08 01:59:42 +0000

    Use toString instead of computing base conversion

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,10 +1,6 @@
 #!/ usr/bin/node
 exports.converter = function (base) {
   return function convert (num) {
-    if (isNaN(parseInt(num / base)) || parseInt(num / base) === 0) {
-      return '0123456789abcdef'[num % base];
-    } else {
-      return '0123456789abcdef'[parseInt(num / base)] + convert(num % base);
-    }
+    return num.toString(base);
   };
 };
