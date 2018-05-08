a24a7dfec4e7c00d1e0ea81928302abb14a3d940
magezil <256@holbertonschool.com>
2018-05-08 01:14:31 +0000

    Fix bugs in conversion

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,16 +1,10 @@
 #!/ usr/bin/node
 exports.converter = function (base) {
-  return function (num) {
-    console.log(convert(num, base));
+  return function convert (num) {
+    if (isNaN(parseInt(num / base)) || parseInt(num / base) === 0) {
+      return "0123456789abcdef"[num % base];
+    } else {
+      return "0123456789abcdef"[parseInt(num / base)] + convert(num % base);
+    }
   }
-}
-
-convert = function (num, base) {
-  if (num === undefined || isNaN(num))
-    return '';
-  if (num < base) {
-    return "0123456789abcdef"[num];
-  }
-console.log(num % base);
-  return "0123456789abcdef"[parseInt(num / base)] + convert(num % base);
 }
