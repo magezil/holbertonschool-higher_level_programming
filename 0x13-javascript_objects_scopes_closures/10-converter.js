60104d7b9b7a1028b8739ddf628c5490b8ea1599
Liz M <256@holbertonschool.com>
2018-05-08 01:53:59 +0000

    Fix semistandard style

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -2,9 +2,9 @@
 exports.converter = function (base) {
   return function convert (num) {
     if (isNaN(parseInt(num / base)) || parseInt(num / base) === 0) {
-      return "0123456789abcdef"[num % base];
+      return '0123456789abcdef'[num % base];
     } else {
-      return "0123456789abcdef"[parseInt(num / base)] + convert(num % base);
+      return '0123456789abcdef'[parseInt(num / base)] + convert(num % base);
     }
-  }
-}
+  };
+};
